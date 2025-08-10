537003008c630e4ddee4b64e65f1205fc0e08cfb
esdringer <thalessousa218@gmail.com>
2025-08-10 01:43:18 -0300

    atualização

--- aula14ex/ex01/interativo.js
+++ aula14ex/ex01/interativo.js
@@ -7,17 +7,32 @@ function clicar() {
     var passo = document.querySelector('#ipas').value
     var resp = document.querySelector('#resp')
     resp.innerHTML = ''
-    var c = 0
+    var c = 1
 
-    if (inicio == 0 || fim == 0 || passo == 0) {
+    if (inicio === '' || fim === '' || passo === '') {
         alert('Complete todos os campos.')
-    } else if (Number(inicio) >= Number(fim)) {
-        alert('O Número inicial é maior ou igual ao Número final. Verifique e tente novamente.')
-    } else if (Number(passo) > (Number(fim) - Number(inicio))) {
-        alert('O passo está muito grande. Diminua e tente novamente.')
-    } else {
-        for (c = Number(inicio); c <= Number(fim); c += Number(passo)) {
-            resp.innerHTML += `${c}...`
-        }
+    } else if (Number(inicio) == Number(fim)) {
+        alert('O valor inicial é igual ao valor final. CORRIJA!')
+    } else if (Number(passo) <= 0) {
+        alert('Passo inválido. Corrija e tente novamente.')
+    } else if (Number(inicio) < Number(fim)) {
+       if (Number(passo) > (Number(fim) - Number(inicio))) {
+            alert('Passo inválido. Corrija e tente novamente.')
+       } else {
+            for (c = Number(inicio); c <= Number(fim); c += Number(passo)) {
+                resp.innerHTML += `${c} \u{1F449}`
+            }
+            resp.innerHTML += `\u{1F3C1}`
+       }
+    } else if (Number(inicio) > Number(fim)) {
+        if (Number(passo) > (Number(inicio) - Number(fim))) {
+            alert('Passo inválido. Corrija e tente novamente.')
+       } else {
+            for (c = Number(inicio); c >= Number(fim); c -= Number(passo)) {
+                resp.innerHTML += `${c} \u{1F449}`
+            }
+            resp.innerHTML += `\u{1F3C1}`
+       }
     }
+    
 }
